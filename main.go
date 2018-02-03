5798802f9dd754e169aebc97d514a7165a6219dc
PǝRV <alexander.w.matulionis@gmail.com>
2018-02-03 12:29:17 -0800

    Almost works

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/mrmiguu/jsutil"
+	"github.com/mrmiguu/sock"
+)
 
+func main() {
+	if err := jsutil.CompileWithGzip(sock.Root + "/script.go"); err != nil {
+		panic(err)
+	}
+
+	sock.Addr = ":80"
+	handshake := sock.Rbool()
+	for range handshake {
+		println("handshake")
+	}
 }
--- www/script.go
+++ www/script.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/mrmiguu/page"
+	"github.com/mrmiguu/page/css"
+	"github.com/mrmiguu/sock"
+)
 
+var (
+	bg    = page.Class("bg")
+	logo  = page.Class("logo")
+	strip = page.Class("strip")
+)
+
+func main() {
+	sock.Addr = "47.148.135.216"
+	handshake := sock.Wbool()
+	handshake <- true
+	for {
+		select {
+		case <-logo.Hit:
+			go logo.Display(css.None)
+			bg.Play()
+		}
+	}
 }
